ec6014e8b1aa864f6891a324fa0ced30461bf9b0
VTrim <vit777ok@gmail.com>
2016-07-02 07:47:05 +0300

    Update Model_offer.php

--- application/models/Model_offer.php
+++ application/models/Model_offer.php
@@ -34,10 +34,10 @@ class Model_offer extends Model {
 		
 		unset($_SESSION['rand_code']);
 		
-		$name =  $this->db->real_escape_string($name);
-		$email = $this->db->real_escape_string($email);
-		$site =  $this->db->real_escape_string($site);
-		$offer = $this->db->real_escape_string($offer);
+		$name =  $this->db->real_escape_string(strip_tags($name));
+		$email = $this->db->real_escape_string(strip_tags($email));
+		$site =  $this->db->real_escape_string(strip_tags($site));
+		$offer = $this->db->real_escape_string(strip_tags($offer));
 
         $this->db->query("INSERT INTO offers SET name = '$name', 
 		                                         email = '$email', 
